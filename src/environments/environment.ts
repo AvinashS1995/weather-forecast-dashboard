9409b4b033dc3350482dff8d7faf463c2e02e443
Avinash Suryawanshi <suryawanshiavinash18@gmail.com>
2025-10-01 22:48:32 +0530

    integrate live backend api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/',
-  // apiUrl: 'https://ems-backend-api.onrender.com/api/',
+  // apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://ems-backend-api.onrender.com/api/',
 };
